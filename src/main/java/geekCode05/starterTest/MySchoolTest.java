e93e231b556a5afdf8d18d37634e280f6789589b
geekXm <zsm787796792@163.com>
2021-12-05 23:36:47 +0800

    更新第五周作业

--- src/main/java/geekCode05/starterTest/MySchoolTest.java
+++ src/main/java/geekCode05/starterTest/MySchoolTest.java
@@ -2,6 +2,11 @@ package geekCode05.starterTest;
 
 import geekCode05.starterTest.starter.MySchool;
 import geekCode05.starterTest.starter.SchoolAutoConfiguration;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = SchoolAutoConfiguration.class)
